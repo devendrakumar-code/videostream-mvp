e390edee80d979018c0eea21aa97ab0b2b53d776
DEVENDRA KUMAR <76100232+devendrakumar-code@users.noreply.github.com>
2025-08-28 23:45:34 +0530

    Add ESLint configuration with TypeScript rulesCreate .eslintrc.js

---
+++ backend/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    '@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  env: {
+    node: true,
+    es2020: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': 'warn',
+    'eqeqeq': ['error', 'always'],
+    'prefer-template': 'error',
+    'object-shorthand': 'error',
+    'array-callback-return': 'error',
+  },
+  ignorePatterns: [
+    'dist/**',
+    'node_modules/**',
+    '*.js',
+    '*.d.ts',
+  ],
+};
